672bc40b306ed1dbe94b9a0c02668a4fa9ec1554
Paul <22284856+ProgramComputer@users.noreply.github.com>
2022-07-13 15:47:03 -0500

    Update predictors.py

--- realworldnlp/predictors.py
+++ realworldnlp/predictors.py
@@ -13,8 +13,7 @@ from typing import List
 class SentenceClassifierPredictor(Predictor):
     def __init__(self, model: Model, dataset_reader: DatasetReader) -> None:
         super().__init__(model, dataset_reader)
-        print(self)
-       # self._tokenizer = dataset_reader._tokenizer or SpacyTokenizer()
+        self._tokenizer = dataset_reader.tokenizer or SpacyTokenizer()
 
     def predict(self, sentence: str) -> JsonDict:
         return self.predict_json({"sentence" : sentence})
